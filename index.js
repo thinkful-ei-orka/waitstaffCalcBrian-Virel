f7b281a05b89118509ec6f2619bb2d925b52af19
bcharles <bcharles41078@gmail.com>
2020-04-28 18:15:31 -0500

    calculations work

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const earningsInfo = {
-    tipTotal: 0.00,
+    tipTotal: 0,
     mealCount: 0,
-    subtotal: 0.00,
-    tip: 0.00,
+    subtotal: 0,
+    tip: 0,
 }
 
 
@@ -13,20 +13,27 @@ function render (whereTo,str){
 }
 
 function averageTip (){
-    return earningsInfo.tipTotal / earningsInfo.mealCount;
+    let calcHold = earningsInfo.tipTotal / earningsInfo.mealCount;
+    return financial(calcHold);
 }
 
 function fncSubtotal (mealPrice, taxRate){
-   earningsInfo.subtotal = mealPrice + taxRate;
+    //console.log(mealPrice, taxRate/100);
+    let calcHold = mealPrice + mealPrice*(taxRate/100);
+   earningsInfo.subtotal = financial(calcHold);
 }
 
 function fncTip (mealPrice, tipPercent){
-    earningsInfo.tip = mealPrice * (tipPercent/100);
+    let calcHold = Number(mealPrice) * (Number(tipPercent)/100);
+    earningsInfo.tip = financial(calcHold);
 }
 
+function financial(x) {
+    return Number.parseFloat(x).toFixed(2);
+  }
 
 function customerCharges(){
-    let total = earningsInfo.subtotal + earningsInfo.tip;
+    let total = Number(earningsInfo.subtotal) + Number(earningsInfo.tip);
     render('.customerCharges', `<p class="js-subtotal">Subtotal: ${earningsInfo.subtotal} </p>
     <p class="js-tip">Tip: ${earningsInfo.tip} </p>
     <hr class="hr12">
@@ -46,6 +53,7 @@ function handleSubmitClick(mealPrice, taxRate, tipPercent){
     fncSubtotal(mealPrice, taxRate);
     fncTip(mealPrice, tipPercent);
     earningsInfo.mealCount += 1;
+    earningsInfo.tipTotal = Number(earningsInfo.tipTotal) + Number(earningsInfo.tip);
     customerCharges();
     myEarningsInfo(); 
 }
@@ -56,21 +64,32 @@ function btnSubmitClick(){
         const mealPrice = $('#mealPriceInput').val();
         const taxRate = $('#taxRateInput').val();
         const tipPercent = $('#tipPercentInput').val();
-        handleSubmitClick(mealPrice, taxRate, tipPercent);
+        handleSubmitClick(Number(mealPrice), Number(taxRate), Number(tipPercent));
     })
 }
 
 function btnCancelClick(){
     $('.meal-Details-Form').on('click', '.Cancel', event =>{
     event.preventDefault();
-    
+    $('#mealPriceInput').val('');
+    $('#taxRateInput').val('');
+    $('#tipPercentInput').val('');
     });
+}
+
+function clearAll (){
+   earningsInfo.tipTotal = 0;
+   earningsInfo.mealCount = 0;
+   earningsInfo.subtotal = 0;
+   earningsInfo.tip = 0;
 }
 
 function btnResetClick(){
     $('.reset-container').on('click', '.resetButton', event =>{
     event.preventDefault();
-    
+    clearAll()
+    customerCharges();
+     $(myEarningsInfo);
     });
 }
 function main (){
