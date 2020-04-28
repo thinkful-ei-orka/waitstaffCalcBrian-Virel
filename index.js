2c4b019cbbd56a83e52f1151d2e2eefb33bdfe43
bcharles <bcharles41078@gmail.com>
2020-04-28 09:56:56 -0500

    wireframe

---
+++ index.js
@@ -0,0 +1,44 @@
+const earningsInfo = {
+    tipTotal: 0.00,
+    mealCount: 0
+}
+
+function render (){
+
+}
+
+function averageTip (){
+
+}
+
+function subtotal (){
+
+}
+
+function tip (){
+
+}
+
+function mealDetail(){
+
+}
+
+function customerCharges(){
+
+}
+
+function myEarningsInfo(){
+
+}
+
+function btnSubmitClick(){
+
+}
+
+function btnCancelClick(){
+
+}
+
+function btnResetClick(){
+
+}
